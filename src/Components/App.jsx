f4e5d4644c1a3f15a560edcb0a8b7d89c98f10ab
Aviv David Malka <sonyon.winx@gmail.com>
2025-08-24 15:25:53 +0300

    Added Footer and Header

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Header from "./Header/Header"
+import Footer from "./Footer/Footer"
 
 
 function App(){
     return(
         <div>
-            <h1>Hello World!</h1>
+            <Header></Header>
+
+            <Footer></Footer>
         </div>
     )
 
